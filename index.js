3c3fe3a07cc2085bc632dabd84e28492bfa2b3d2
Lucas Dobbins <lucasdobbins@gmail.com>
2015-07-17 17:30:02 +1000

    updated shit

--- index.js
+++ index.js
@@ -6,8 +6,34 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
     console.log('a user connected');
+
+    socket.on('disconnect', function() {
+        console.log('a user disconnected')
+    });
+
+    socket.on('player1', function(player) {
+        console.log(player);
+        io.emit('player1');
+    });
+
+    socket.on('player2', function(player) {
+        console.log(player);
+        io.emit('player2');
+    });
+
+    socket.on('player3', function(player) {
+        console.log(player);
+        io.emit('player3');
+    });
+
+    socket.on('reset', function() {
+        console.log("reset");
+        io.emit('reset');
+    })
+
+
 });
 
 http.listen('3000', function() {
